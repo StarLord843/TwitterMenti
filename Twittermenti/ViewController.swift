8da67f0079580e2317b03c500ad2c812b50b938a
Kaushal <kkaushal849@gmail.com>
2020-10-22 12:33:04 +0530

    Initial working project

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetSentimentClassifier()
+    
+    let swifter = Swifter(consumerKey: "EEOH5KOZvoo4U94Ljqyy3Hre7", consumerSecret: "AlRY6A6qVAVGe4YhLXu0olKbucg6kFIvM9Tc5owsmd2C7khyL8")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            
+            
+            
+        } failure: { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
+        
         
     }
 
